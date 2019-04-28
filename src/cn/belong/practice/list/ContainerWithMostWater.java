ac8c44380622964109c3b75bf6fdfbb310098089
shiwen.chen <shiwen.chen@56qq.com>
2019-04-28 23:02:24 +0800

    leetcode NO 11. Container With Most Water manner 2

--- src/cn/belong/practice/list/ContainerWithMostWater.java
+++ src/cn/belong/practice/list/ContainerWithMostWater.java
@@ -10,14 +10,14 @@ package cn.belong.practice.list;
 public class ContainerWithMostWater {
 
     /**
-     * Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+     * Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate
+     * (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and
+     * (i, 0). Find two lines, which together with x-axis forms a container, such that the container
+     * contains the most water.
      * <p>
      * Note: You may not slant the container and n is at least 2.
-     *
-     * @param height
-     * @return
      */
-    public int maxArea(int[] height) {
+    public int maxArea01(int[] height) {
 
         int maxContainer = 0;
 
@@ -27,6 +27,25 @@ public class ContainerWithMostWater {
                 int hj = height[j];
                 maxContainer = Math.max(Math.min(hi, hj) * (j - i), maxContainer);
             }
+        }
+        return maxContainer;
+    }
+
+    public int maxArea(int[] height) {
+        int low = 0;
+        int high = height.length - 1;
+        int maxContainer = 0;
+        while (low < high) {
+            int curContainer;
+            int width = high - low;
+            if (height[low] < height[high]) {
+                curContainer = height[low] * width;
+                low++;
+            } else {
+                curContainer = height[high] * width;
+                high--;
+            }
+            maxContainer = Math.max(curContainer, maxContainer);
         }
         return maxContainer;
     }
